338203c11be95837424f93c6cb8dbe52fb8ecc3d
Kirsman <akirsma@amazon.com>
2020-04-29 22:12:30 -0300

    bucket name change

--- website/js/config.js
+++ website/js/config.js
@@ -1,11 +1,11 @@
 // https://forums.aws.amazon.com/message.jspa?messageID=757990#757990
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_nAWcMatR6', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'hkqb90k3d8d8scl48gi7hqt4g', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId: '[USERPOOLID]', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '[USERPOOLCLIENTID]', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: '[REGION]' // e.g. us-east-1
     },
     api: {
-        invokeUrl: 'https://5n49ga2kqk.execute-api.us-east-1.amazonaws.com/dev' // e.g. https://01gfag2l88.execute-api.us-east-1.amazonaws.com/dev,
+        invokeUrl: '[APIGATEWAYSTAGEURL]' // e.g. https://01gfag2l88.execute-api.us-east-1.amazonaws.com/dev,
     }
 };
